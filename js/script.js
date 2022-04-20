114dce056d3073bcebe8398b41f5d055f17a6879
kaua00112 <kaua824@outlook.com>
2022-04-20 09:23:46 -0300

    atualização slide

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-function menutoggle() {
-    let menuArea = document.getElementById('menu-area');
-    
-    if(menuArea.style.width == '200px'){
-        menuArea.style.width = '0px';
-    }else{
-        menuArea.style.width = '200px';
+let totalslides = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
+document.querySelector('.slider--width').style.width = `calc(100vw * ${totalslides})`;
+
+
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalslides - 1;
     }
+    updateMargin();
 }
+
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalslides - 1)){
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+function updateMargin() {
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goNext, 4000);
